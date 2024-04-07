ebf96971ff3a88a3680af3f847edf39c560b7c7c
romikkimor123@gmail.com <romikkimor123@gmail.com>
2024-04-07 12:29:54 +0300

    Получить коллекцию инсталяций и добавить инсталяцию

--- src/Fillial.Api/Controllers/V1/InstallationsController.cs
+++ src/Fillial.Api/Controllers/V1/InstallationsController.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using PFilial.API.Requests;
 using PFilial.API.Responses.V1;
 using PFilial.BLL.Services.Interfaces;
 using PrinterFil.Api.Models;
+using System.ComponentModel.DataAnnotations;
 
 namespace PFilial.API.Controllers.V1;
 
@@ -15,8 +17,8 @@ public class InstallationsController : ControllerBase
 		_installationsService = installationsService;
 	}
 
-	[HttpGet]
-	public async Task<ActionResult<Installation[]>> GetInstallations([FromQuery] int? filialId)
+	[HttpGet("collection")]
+	public async Task<ActionResult<Installation[]>> GetCollectionAsync([FromQuery] int? filialId)
 	{
 		InstallationModel [] installations = await _installationsService.GetCollectionAsync(filialId);
 
@@ -29,5 +31,47 @@ public class InstallationsController : ControllerBase
 				x.IsDefault,
 				x.Order))
 			.ToArray());
+	}
+
+	[HttpGet]
+	public async Task<ActionResult<Installation>> GetByIdAsync([Required][FromQuery] int id)
+	{
+		InstallationModel? instal = await _installationsService.GetAsync(id);
+
+		if (instal == null)
+		{
+			return NotFound();
+		}
+
+		return Ok(new Installation(
+				instal.Id,
+				instal.Name,
+				instal.FilialId,
+				instal.PrintingDeviceId,
+				instal.IsDefault,
+				instal.Order)
+			);
+	}
+
+	[HttpPost]
+	public async Task<ActionResult<int>> AddAsync(AddInstallationRequest request)
+	{
+		InstallationModel installation = new(
+			0,
+			request.Name,
+			request.FilialId,
+			request.DeviceId,
+			request.IsDefault,
+			request.Order ?? 0);
+
+		int? id = await _installationsService.AddAsync(installation);
+
+		if (id == null)
+		{
+			return NotFound();
+		}
+
+		return Ok(id);
+
 	}
 }
